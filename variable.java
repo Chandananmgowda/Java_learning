fc7d74bdc52500a800d21f3eef7c86d1ca327514
CHANDANA N M <chandananmgowda1616.com>
2024-09-25 01:18:35 +0530

    5rd commit

---
+++ variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    public static void main(String[] args){
+       int num1=10;
+       int num2=5;
+       int result1=num1+num2;
+       int result2=num1-num2;
+       int result3=num1*num2;
+       int result4=num1/num2;
+       System.out.println(result1);
+       System.out.println(result2);
+       System.out.println(result3);
+       System.out.println(result4);
+
+
+    }
+}
