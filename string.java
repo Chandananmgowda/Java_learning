297596be0a50c3292f435ac2bad5142b7e04d811
CHANDANA N M <chandananmgowda1616.com>
2024-09-25 01:07:18 +0530

    4rd commit

--- string.java
+++ string.java
@@ -1,5 +1,8 @@
 public class string {
     public static void main(String[] args){
         System.out.println(2+3);
+        System.out.println(2-3);
+        System.out.println(2*3);
+        System.out.println(6/3);
     }
 }
