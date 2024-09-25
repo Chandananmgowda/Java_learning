4a6a8f7c9de2393f9d575829e83106767a2c3f6b
CHANDANA N M <chandananmgowda1616.com>
2024-09-25 21:57:53 +0530

    8th commit

---
+++ relationalop.java
@@ -0,0 +1,9 @@
+public class relationalop {
+    public static void main(String[] args) {
+        int a=10;
+        int b=15;
+
+        boolean c= a > b;
+        System.out.println(c);
+    }
+}
