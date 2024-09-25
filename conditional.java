19fb24f5ab91888d8898174e657fd6190aa9b226
CHANDANA N M <chandananmgowda1616.com>
2024-09-26 00:25:36 +0530

    10th commit

---
+++ conditional.java
@@ -0,0 +1,14 @@
+public class conditional {
+    public static void main(String[] args) {
+        int x=19;
+        int y=10;
+        int z=13;
+
+        if(x>y && x>z)
+        System.out.println(x);
+        else if(x<y && z<y)
+        System.out.println(y);
+        else
+        System.out.println(z);
+    }
+}
