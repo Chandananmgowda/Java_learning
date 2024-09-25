2e9e9ebbdd185de3e8f549e4072f75d16e7b8cd2
CHANDANA N M <chandananmgowda1616.com>
2024-09-26 00:59:59 +0530

    new commit

--- casting.java
+++ casting.java
@@ -1,8 +1,8 @@
 public class casting {
     public static void main(String[] args) {
         int a=236;
-        byte b=(byte)a;
-    System.out.println(a);
+        byte b =(byte)a;
+    System.out.println(b);
     byte r =127;
     int p=r;
     System.out.println(p);
